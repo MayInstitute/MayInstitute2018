ab2ff5906ea81597586cc4e1694b3cb490a1e918
Laurent <lg390@cam.ac.uk>
2018-04-26 14:28:29 +0100

    add iprg data with NAs

--- Program3_IntroR/data/make_iprg.R
+++ Program3_IntroR/data/make_iprg.R
@@ -3,3 +3,11 @@ iprg$TechReplicate <-
     factor(sub("\\.raw", "", sub(".+sample.+_", "", sub("-", "_", iprg$Run))))
 save(iprg, file = "iprg.rda")
 write.csv(iprg, file = "iPRG_example_runsummary.csv", row.names = FALSE)
+
+
+set.seed(123)
+i <- sample(nrow(iprg), 1e3)
+iprgna <- iprg
+iprgna[i, "Intensity"] <- NA
+iprgna[i, "Log2Intensity"] <- NA
+save(iprgna, file = "iprgna.rda")
